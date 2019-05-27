bd9af79a7b176bd5aaec00d709ceb1094e6cf539
Francesco Tabbò <francesco.tabbo2000@gmail.com>
2019-05-27 11:15:40 +0000

    Sagheddu dice going on

--- Server/app/app.js
+++ Server/app/app.js
@@ -12,6 +12,59 @@ app.set('view engine', 'pug');*/
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.post('/login', function(req, res){
+    console.log("uno");
+    console.log(req.body);
+    var conn = mysql.createConnection({
+        host: "remotemysql.com",
+        user: "JPQ7c5oklq",
+        password: "g4JgzatqTc",
+        database: "JPQ7c5oklq"
+    });
+    var nickname = req.body.nickname;
+    var password = req.body.password;
+
+    conn.connect(function(err) {
+        console.log("due");
+        if (err) throw err;
+        console.log("Connected!");
+        conn.query("SELECT * FROM User WHERE user = '" + nickname + "' AND password ='" + password + "'" ,function(errr, fields){
+            if (errr) throw errr;
+            console.log("tre");
+            res.send(fields);
+        });
+    });
+});
+
+app.post('/register', function(req, res){
+    console.log("uno");
+    console.log(req.body);
+    var conn = mysql.createConnection({
+        host: "remotemysql.com",
+        user: "JPQ7c5oklq",
+        password: "g4JgzatqTc",
+        database: "JPQ7c5oklq"
+    });
+    var nickname = req.body.nickname;
+    var password = req.body.password;
+    var nome =req.body.name;
+    var cognome =req.body.cognome;
+    var email =req.body.email;
+    var indirizzo =req.body.indirizzo;
+    var telefono =req.body.telefono;
+
+    conn.connect(function(err) {
+        console.log("due");
+        if (err) throw err;
+        console.log("Connected!");
+        conn.query("INSERT INTO User(nome, cognome, email, password, user, indirizzo, telefono) VALUES( '" + nome +"', '" + cognome + "', '" + email + "', '" + password + "', '" + nickname + "', '" + indirizzo + "', '" + telefono + "')"  ,function(errr, fields){
+            if (errr) throw errr;
+            console.log("tre");
+            res.send(fields);
+        });
+    });
+});
+
 app.get('/visualizza', function (req, res) {
     var args = {};
     client.get("https://3000-df7cac6b-cc76-41e1-a520-ee0ac7652c80.ws-eu0.gitpod.io/Visualizza", args, function (data, ress) {
@@ -70,3 +123,5 @@ app.post('/takeOff', function (req, res) {
             res.send([{message: 'KO'}]);
     });
 });
+
+app.listen(3000, function(){});
